d6e47ef1c68724d195d2a97e3a59ffa600fdc5f1
Sobir Alimov <sobiriddin@gmail.com>
2022-12-03 21:14:11 +0500

    second

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 import datetime
-import csv
 from collections import Counter
-
 
 url = 'https://kun.uz/uz/news/category/jahon'
 res = requests.get(url)
@@ -18,24 +16,19 @@ for news in news_:
 
     url = f'https://kun.uz{link}'
     res = requests.get(url)
+
     soup = BeautifulSoup(res.content,'html.parser')
-
     content = soup.find('div', class_='single-content').text
-
     time = datetime.datetime.now()
-
     words = content.split()
 
     Counter_ = Counter(words)
-
     most_occur = Counter_.most_common(5)
-
 
 
     info.append([link, time, content, words, most_occur])
 
 
 columns = ["Link", "Time",  "Content", "Words", "Most occured words"]  #Columns for dataset 
-
 df = pd.DataFrame(data=info, columns=columns)
 df.to_csv('news.csv')
