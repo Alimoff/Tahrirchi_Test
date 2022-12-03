a7b1da6593aece057ef27ca50374102e12811ed2
Sobir Alimov <sobiriddin@gmail.com>
2022-12-03 21:09:22 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+import datetime
+import csv
+from collections import Counter
+
+
+url = 'https://kun.uz/uz/news/category/jahon'
+res = requests.get(url)
+
+soup = BeautifulSoup(res.content,'html.parser')
+news_= soup.find_all('a', class_='small-news__title')
+
+info= []
+for news in news_:
+    link = news["href"]
+
+    url = f'https://kun.uz{link}'
+    res = requests.get(url)
+    soup = BeautifulSoup(res.content,'html.parser')
+
+    content = soup.find('div', class_='single-content').text
+
+    time = datetime.datetime.now()
+
+    words = content.split()
+
+    Counter_ = Counter(words)
+
+    most_occur = Counter_.most_common(5)
+
+
+
+    info.append([link, time, content, words, most_occur])
+
+
+columns = ["Link", "Time",  "Content", "Words", "Most occured words"]  #Columns for dataset 
+
+df = pd.DataFrame(data=info, columns=columns)
+df.to_csv('news.csv')
